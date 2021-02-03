d328676c7e6384149bf62e900f1643cc81b45bd9
Garret Sugarbaker <gsugarbaker@gmail.com>
2021-02-03 15:24:44 -0800

    Update README and documentation in code.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-catkin_ws
+### Project 1 Part 2-3
+Garret Sugarbaker
+
+#### Installation
+No additional Python packages are needed.
+Add the `src/du_garret_sugarbaker` directory to your Catkin workspace.
+Source your Catkin workspace's `devel/setup.sh`
+Run `catkin_make` in the root directory of your Catkin workspace.
+Start roscore by running `roscore` in a new terminal.
+Start turtlesim by running `rosrun turtlesim turtlesim_node` in another terminal.
+Call `rosrun du_garret_sugarbaker du_garret_sugarbaker.py`
+
+
+#### Tuning Variables
+The script uses global variables to store settings for having the turtle follow a path.
+The goal of the script is to have the turtle follow the path in as close to the target time as possible.
+|Variable|Description|
+|--------|-----------|
+|`runtime_seconds`|Desired runtime in seconds to be met.|
+|`vertices`|An ordered list of relative positions to have the turtle follow. The turtle 'starts' at the first vertex.|
+|`shape_scale`|A scale to be applied to the shape. Vertices components are multiplied by this scalar|
+|`tick_rate`|Rate at which velocity updates are published|
--- src/du_garret_sugarbaker/src/du_garret_sugarbaker.py
+++ src/du_garret_sugarbaker/src/du_garret_sugarbaker.py
@@ -74,32 +74,39 @@ class SegmentInfo:
 
 # estimated vertices for the D shape, will be used to compute velocities
 vertices = [Vec3(0,0,0), Vec3(9,0,0), Vec3(10,-1,0), Vec3(10,-9,0), Vec3(9,-10,0), Vec3(0,-10,0), Vec3(0,-9,0), Vec3(.5,-9,0), Vec3(.5,-1,0), Vec3(0,-1,0), Vec3(0,0,0)]
+# the scale of the shape being drawn
+shape_scale = 20
+# how many velocity updates occur each second
+tick_rate = 50.0
+# the length of each velocity udpate
+tick_time = 1.0/tick_rate
+# the desired runtime in seconds
+runtime_seconds = 10
+
+
+rospy.loginfo("ticktick is {}".format(tick_time))
 
 if __name__ == '__main__':
     pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     rospy.init_node('du_garret_sugarbaker_d_velocity', anonymous=True)
 
-    shape_scale = 20
-
     for i in range(0, len(vertices)):
         vertices[i] *= shape_scale
 
-    tick_rate = 50.0
-    tick_time = 1.0/tick_rate
-    rospy.loginfo("ticktick is {}".format(tick_time))
     rate = rospy.Rate(tick_rate)
 
-    # adjust for 1 minute runtime, runtime will be less than or equal to goal
-    total_ticks_goal = 10 * tick_rate
+    # adjust for runtime, runtime will be less than or equal to goal
+    total_ticks_goal = runtime_seconds * tick_rate
     # calculate total travel distance to use in deriving segment tick counts
     _total_linear_dist = total_linear_dist(vertices)
-
+    # calculate linear speed in order to make runtime less than or equal to goal length
     linear_speed = _total_linear_dist / total_ticks_goal * tick_rate
 
     rospy.loginfo("computed linear speed {}".format(linear_speed))
 
     segment_index = 0
 
+    # returns the next SegmentInfo object to use
     def get_next_segment():
         global segment_index
         start_pos = vertices[segment_index]
@@ -114,11 +121,14 @@ if __name__ == '__main__':
     current_segment = get_next_segment()
 
     while not rospy.is_shutdown():
+        # if the segment is complete get a new one
         if current_segment.is_complete():
+            # if no new segments exist terminate program
             if segment_index == len(vertices):
                 break
             current_segment = get_next_segment()
         
+        # get current linear velocity to update
         vel = current_segment.vel(tick_time, linear_speed)
 
         msg = Twist()
@@ -130,6 +140,7 @@ if __name__ == '__main__':
         #rospy.loginfo("publishing twist " + str(msg))
 
         current_segment.tick()
+        #publish twist
         pub.publish(msg)
         rate.sleep()
 
