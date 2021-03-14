b53b7464ff473f9a088aba79cd9e561fae3728b9
Garret Sugarbaker <gsugarbaker@gmail.com>
2021-03-13 17:40:52 -0800

    Add to Project 3 Task 2

--- src/project3_wall_following/scripts/wallfollow.py
+++ src/project3_wall_following/scripts/wallfollow.py
@@ -3,8 +3,11 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from std_srvs.srv import Empty
 import math
 import os
+import pickle
+import random
 
 if __name__ == '__main__':
     rospy.init_node('project3_wall_following', anonymous=True)
@@ -184,6 +187,21 @@ if __name__ == '__main__':
             for state_ordinal, ele in enumerate(q_table)])
         return message
 
+    def get_local_file_path(filename):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), filename)
+
+    def write_table_to_file(filename):
+        global q_table
+        qtable_file = open(filename, "wb")
+        pickle.dump(q_table, qtable_file)
+        qtable_file.close()
+
+    def read_table_file(filename):
+        global q_table
+        qtable_file = open(filename, "rb")
+        q_table = pickle.load(qtable_file)
+        qtable_file.close()
+
     #debug_q_table(q_table)
     #q_policy({
     #    RIGHT: TOO_FAR,
@@ -194,103 +212,109 @@ if __name__ == '__main__':
     # TODO: REMOVE TO END DEBUG
     #exit()
 
-    # set default
-    # matches all states and increments q-values for FORWARD actions by 1
-    q_policy({}, FORWARD, .5, 0)
+    qtable_path = get_local_file_path("q-table.dat")
+    if not os.path.exists(qtable_path):
+        # set default
+        # matches all states and increments q-values for FORWARD actions by 1
+        q_policy({}, FORWARD, .5, 0)
 
-    # handle obstacle in-front, concave corner
-    q_policy({
-        FRONT: TOO_CLOSE,
-        RIGHT: CORRECT
-    }, LEFT_I, 1, 0)
+        # handle obstacle in-front, concave corner
+        q_policy({
+            FRONT: TOO_CLOSE,
+            RIGHT: CORRECT
+        }, LEFT_I, 1, 0)
 
-    # handle obstacle in-front, concave corner, too close
-    q_policy({
-        FRONT: TOO_CLOSE,
-        RIGHT: TOO_CLOSE
-    }, LEFT_I, 1, 0)
+        # handle obstacle in-front, concave corner, too close
+        q_policy({
+            FRONT: TOO_CLOSE,
+            RIGHT: TOO_CLOSE
+        }, LEFT_I, 1, 0)
 
-    # in corner
-    q_policy({
-        FRONT: TOO_CLOSE,
-        RIGHT: TOO_CLOSE,
-        FRONT_RIGHT: TOO_CLOSE
-    }, LEFT_I, 1, 0)
+        # in corner
+        q_policy({
+            FRONT: TOO_CLOSE,
+            RIGHT: TOO_CLOSE,
+            FRONT_RIGHT: TOO_CLOSE
+        }, LEFT_I, 1, 0)
 
-    # is facing slightly too right
-    q_policy({
-        RIGHT: TOO_CLOSE,
-        FRONT_RIGHT: CORRECT
-    }, SLIGHT_LEFT_M, 1, 0)
+        # is facing slightly too right
+        q_policy({
+            RIGHT: TOO_CLOSE,
+            FRONT_RIGHT: CORRECT
+        }, SLIGHT_LEFT_M, 1, 0)
 
-    # is facing much too right
-    q_policy({
-        RIGHT: TOO_CLOSE,
-        FRONT_RIGHT: TOO_CLOSE
-    }, LEFT_M, 1, 0)
+        # is facing much too right
+        q_policy({
+            RIGHT: TOO_CLOSE,
+            FRONT_RIGHT: TOO_CLOSE
+        }, LEFT_M, 1, 0)
 
-    # is facing too left
-    q_policy({
-        RIGHT: TOO_FAR,
-        FRONT_RIGHT: TOO_FAR
-    }, RIGHT_M, 1, 0)
+        # is facing too left
+        q_policy({
+            RIGHT: TOO_FAR,
+            FRONT_RIGHT: TOO_FAR
+        }, RIGHT_M, 1, 0)
 
-    # is facing too left
-    q_policy({
-        RIGHT: TOO_FAR,
-        FRONT_RIGHT: CORRECT
-    }, RIGHT_M, 1, 0)
+        # is facing too left
+        q_policy({
+            RIGHT: TOO_FAR,
+            FRONT_RIGHT: CORRECT
+        }, RIGHT_M, 1, 0)
 
-    # is misplaced (cannot see wall)
-    # spin and try to find wall. since this is probably due to it
-    # turning too early we will look right
-    # TODO: make move forward to keep from getting stuck??
-    q_policy({
-        RIGHT: TOO_FAR,
-        FRONT_RIGHT: TOO_FAR,
-        FRONT: TOO_FAR
-    }, SLIGHT_RIGHT_M, 1, 0)
+        # is misplaced (cannot see wall)
+        # spin and try to find wall. since this is probably due to it
+        # turning too early we will look right
+        # TODO: make move forward to keep from getting stuck??
+        q_policy({
+            RIGHT: TOO_FAR,
+            FRONT_RIGHT: TOO_FAR,
+            FRONT: TOO_FAR
+        }, SLIGHT_RIGHT_M, 1, 0)
 
-    # wall is ahead, not to side
-    q_policy({
-        RIGHT: TOO_FAR,
-        FRONT: CORRECT
-    }, SLIGHT_LEFT_M, 1, 0)
+        # wall is ahead, not to side
+        q_policy({
+            RIGHT: TOO_FAR,
+            FRONT: CORRECT
+        }, SLIGHT_LEFT_M, 1, 0)
 
-    # wall is ahead, too close, not to side
-    q_policy({
-        RIGHT: TOO_FAR,
-        FRONT: TOO_CLOSE
-    }, SLIGHT_LEFT_I, 1, 0)
+        # wall is ahead, too close, not to side
+        q_policy({
+            RIGHT: TOO_FAR,
+            FRONT: TOO_CLOSE
+        }, SLIGHT_LEFT_I, 1, 0)
 
-    # approaching convex corner
-    q_policy({
-        RIGHT: CORRECT,
-        FRONT: TOO_FAR,
-        FRONT_RIGHT: TOO_FAR
-    }, SLIGHT_RIGHT_M, 1, 0)
+        # approaching convex corner
+        q_policy({
+            RIGHT: CORRECT,
+            FRONT: TOO_FAR,
+            FRONT_RIGHT: TOO_FAR
+        }, SLIGHT_RIGHT_M, 1, 0)
 
-    # approaching convex corner too close
-    q_policy({
-        RIGHT: TOO_CLOSE,
-        FRONT: TOO_FAR,
-        FRONT_RIGHT: TOO_FAR
-    }, FORWARD, 1, 0)
+        # approaching convex corner too close
+        q_policy({
+            RIGHT: TOO_CLOSE,
+            FRONT: TOO_FAR,
+            FRONT_RIGHT: TOO_FAR
+        }, FORWARD, 1, 0)
 
-    # stuck on convex corner
-    q_policy({
-        RIGHT: TOO_FAR,
-        FRONT: TOO_FAR,
-        FRONT_RIGHT: TOO_CLOSE
-    }, SLIGHT_LEFT_M, 2, 0)
+        # stuck on convex corner
+        q_policy({
+            RIGHT: TOO_FAR,
+            FRONT: TOO_FAR,
+            FRONT_RIGHT: TOO_CLOSE
+        }, SLIGHT_LEFT_M, 2, 0)
 
-    f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "q-table.txt"), "w")
+        write_table_to_file(qtable_path)
+    read_table_file(qtable_path)
+
+    f = open(get_local_file_path("q-table.txt"), "w")
     f.write("q-values : state\n" + debug_q_table(q_table))
     f.write("\nindices correspond to {}".format(debug_action_names))
     f.close()
 
     last_state = None
     last_state_changed = False
+    last_distances = None
 
     def on_laser_scan(data):
         #rospy.loginfo("Received laser data with {} points.".format(len(data.ranges)))
@@ -307,7 +331,9 @@ if __name__ == '__main__':
             raise Exception('Could not find angle {}'.format(rad))
 
         global last_state
-        last_state = state_of([get_at_angle(rad) for rad in probed_angles])
+        global last_distances
+        last_distances = [get_at_angle(rad) for rad in probed_angles]
+        last_state = state_of(last_distances)
         global last_state_changed
         last_state_changed = True
 
@@ -322,6 +348,9 @@ if __name__ == '__main__':
                 debug_substate_names[distance_ordinal])
         rospy.loginfo(message)
 
+    def get_random_action():
+        return possible_actions[random.randrange(len(possible_actions))]
+
     # get Twist of best action based on state and q_table
     def get_best_action(state):
         actions_q_values = q_table[possible_states.index(state)]
@@ -333,17 +362,148 @@ if __name__ == '__main__':
 
         return max_action[1]
 
-    while not rospy.is_shutdown():
+    wait_time = .1
+
+    elapsed_realtime = 0
+    reset_simulation_callable = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+
+    rate = rospy.Rate(5)
+
+    wall_start_time = 0
+    wall_travel_dist = 0
+    away_start_time = 0
+    last_save_time = 0
+    prior_state = None
+
+    wall_reward_per_time = .1
+
+    max_bad_dist_time = 1
+    min_el_bad_dist_time = 5
+
+    is_by_wall = False
+
+    epsilon = .1
+    alpha = .3
+    gamma = .5
+
+    save_interval = 2
+
+    def reward_action(last_state, next_state, action, reward):
+        global q_table
+        def get_qvalue(s, a):
+            return q_table[possible_states.index(s)][possible_actions.index(a)]
+        def set_qvalue(s, a, v):
+            global q_table
+            q_table[possible_states.index(s)][possible_actions.index(a)] = v
+        def max_reward(state):
+            m = 0
+            for r in q_table[possible_states.index(state)]:
+                m = max(r, m)
+            return m
+        set_qvalue(last_state, action, get_qvalue(last_state, action) + alpha * (reward + gamma * max_reward(next_state)) - get_qvalue(last_state, action))
+
+    def correct_distance(distances):
+        def close():
+            # if the robot is too close on any dimension
+            thresh = ideal_wall_dist * (1 - wall_dist_tol) * .75
+
+            return distances[0] < thresh or distances[1] < thresh or distances[2] < thresh
+        def far():
+            # if the robot is not close enough on any side (lost wall)
+            thresh = ideal_wall_dist * (1 + wall_dist_tol)
+            return distances[2] > thresh
+        return not (close() or far())
+
+    def get_current_reward():
+        if is_by_wall:
+            return wall_reward_per_time * wall_travel_dist
+        #else:
+            #return -wall_reward_per_time * (elapsed_realtime - away_start_time)
+        #return -wall_reward_per_time
+        return 0
+
+    def reset_simulation():
+        global wall_start_time
+        global away_start_time
+        global elapsed_realtime
+        global last_save_time
+        global is_by_wall
+        global prior_state
+        global last_state
+        global last_distances
         global last_state_changed
+        global wall_travel_dist 
+
+        wall_start_time = 0
+        wall_travel_dist = 0
+        away_start_time = 0
+        elapsed_realtime = 0
+        last_save_time = 0
+        is_by_wall = False
+        prior_state = None
+        last_state = None
+        last_distances = None
+        last_state_changed = False
+        reset_simulation_callable()
+
+    action = None
+
+    distance_travelling_actions = [FORWARD, RIGHT_M, SLIGHT_RIGHT_M, LEFT_M, SLIGHT_LEFT_M]
+
+    while not rospy.is_shutdown():
+        if elapsed_realtime - last_save_time >= save_interval:
+            write_table_to_file(qtable_path)
+            rospy.loginfo("Saving Q-Table")
+            last_save_time = elapsed_realtime
+
+        global last_state_changed
+        global action
+        global prior_state
         if last_state_changed:
             debug_state(last_state)
             last_state_changed = False
-            action = get_best_action(last_state)
+
+            if not prior_state == None:
+                reward = get_current_reward()
+                rospy.loginfo("\tReward is {}".format(reward))
+                reward_action(prior_state, last_state, action, reward)
+
+            if correct_distance(last_distances):
+                if not is_by_wall:
+                    is_by_wall = True
+                    wall_start_time = elapsed_realtime
+                    wall_travel_dist = 0
+            else:
+                if is_by_wall:
+                    is_by_wall = False
+                    away_start_time = elapsed_realtime
+                    wall_travel_dist = 0
+                elif elapsed_realtime > min_el_bad_dist_time and elapsed_realtime - away_start_time > max_bad_dist_time:
+                    reset_simulation()
+                    continue
+            
+            action = None
+
+            if random.random() < epsilon:
+                action = get_random_action()
+            else:
+                action = get_best_action(last_state)
+
+            wall_travel_dist *= .8
+            if is_by_wall and action in distance_travelling_actions:
+                wall_travel_dist += 1
+            
 
             rospy.loginfo("Chose state {}".format(debug_action_names[possible_actions.index(action)]))
 
             vel_publisher.publish(action)
-        else:
-            rospy.loginfo("Did not receive a laser scan since last action.")
 
-        rospy.sleep(.5)
+            try:
+                rate.sleep()
+            except:
+                rospy.loginfo("Simulation reset")
+            elapsed_realtime += wait_time
+            prior_state = last_state
+
+        #else:
+            #rospy.loginfo("Did not receive a laser scan since last action.")
